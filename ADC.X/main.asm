7c0d6ae53863b55046b9da9d6bf02ba9fb4f94fd
Gongo <jvilla1714@gmail.com>
2019-06-22 18:26:53 -0400

    Added more ports to display remaining bits

--- ADC.X/main.asm
+++ ADC.X/main.asm
@@ -11,37 +11,69 @@ org 0; start code at 0
     RESULTLO EQU 0x01
     Delay1 EQU 0x02
     Delay2 EQU 0x03
+    offset EQU 0x04
     GOTO Start
 Lookup:
+    MULWF offset
+    MOVF PRODL, 0
     ADDWF PCL
+    RETLW 0xD1; F
+    RETLW 0x51; E
+    RETLW 0x06; D
+    RETLW 0x59; C
+    RETLW 0x43; B
+    RETLW 0xC0; A
+    RETLW 0xE0; 9
+    RETLW 0x00; 8
+    RETLW 0xEC; 7
+    RETLW 0x41; 6
+    RETLW 0x61; 5
+    RETLW 0xE2; 4
+    RETLW 0x64; 3
+    RETLW 0x54; 2
+    RETLW 0xEE; 1
+    RETLW 0x08; 0
+LookupReversed:
+    MULWF offset
+    MOVF PRODL, 0
+    ADDWF PCL
+    RETLW 0xD1; F
+    RETLW 0x51; E
+    RETLW 0x06; D
+    RETLW 0x59; C
+    RETLW 0x43; B
+    RETLW 0xC0; A
+    RETLW 0xE0; 9
+    RETLW 0x00; 8
+    RETLW 0xEC; 7
+    RETLW 0x41; 6
+    RETLW 0x61; 5
+    RETLW 0xE2; 4
     RETLW 0x08; 0
     RETLW 0xEE; 1
     RETLW 0x54; 2
     RETLW 0x64; 3
-    RETLW 0xE2; 4
-    RETLW 0x61; 5
-    RETLW 0x41; 6
-    RETLW 0xEC; 7
-    RETLW 0x00; 8
-    RETLW 0xE0; 9
-    RETLW 0xC0; A
-    RETLW 0x43; B
-    RETLW 0x59; C
-    RETLW 0x06; D
-    RETLW 0x51; E
-    RETLW 0xD1; F
 Start:
     CLRF TRISA
     CLRF TRISB
+    CLRF TRISC
+    CLRF TRISD
     CLRF PORTA
     CLRF PORTB
+    CLRF PORTC
+    CLRF PORTD
     CLRF LATB
     CLRF LATA
+    CLRF LATC
+    CLRF LATD
     CLRF RESULTHI
     CLRF RESULTLO
     CLRF Delay1
     CLRF Delay2
+    CLRF offset
 Test:
+    MOVLW d'2'
+    MOVWF offset
     MOVLW B'10101111'
     MOVWF ADCON2
     MOVLW B'00000000'
@@ -56,11 +88,28 @@ ADCPoll:
     BRA ADCPoll
     MOVFF ADRESH, RESULTHI
     MOVFF ADRESL, RESULTLO
-    COMF RESULTLO; Complement bits since 7 segment is ACTIVE LOW
-    MOVF RESULTLO, w
-    ANDLW b'00011111'; Strip off high bits
-    CALL Lookup; Call the lookup table
+    COMF RESULTHI
+    MOVF RESULTHI, w
+    ANDLW b'00001111'; Strip off high bits
+    CALL LookupReversed; Call the lookup table
     MOVWF PORTB; Store the output pattern on the ports
+    MOVF RESULTLO, w; Upper half of the reult
+    ANDLW b'00001111'; Strip off high bits
+    CALL Lookup; Call the lookup table
+    MOVWF PORTD; Store the output pattern on the ports
+    MOVFF ADRESL, RESULTLO
+    SWAPF RESULTLO, 1; Get the higher bits
+    MOVF RESULTLO, w
+    ANDLW b'00001111'; strip off high bits
+    CALL Lookup
+    MOVWF PORTC; store values on port C
+    MOVFF ADRESL, RESULTLO
+    SWAPF RESULTLO, 1; Get high bits
+    COMF RESULTLO
+    MOVF RESULTLO, w
+    ANDLW b'00001111'
+    CALL Lookup
+    MOVWF PORTA; Remaining bits on port A
 Delay:
     DECFSZ Delay1, 1
     GOTO Delay
